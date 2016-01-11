3d98075d23fb05c9036d814276b0ed29903d8520
jairorudasp <jairo.enrique@celulardireto.com.br>
2016-01-11 17:22:57 -0200

    Inicio da explicação de funções iniciada.

--- portugues/5-funções/funcoesglobais.js
+++ portugues/5-funções/funcoesglobais.js
@@ -1,3 +1,22 @@
 /*
+    Funções e Funções Globais
 
+    Uma definição bem generica do que é uma função, poderia ser: O trecho de um codigo que se repetem em diferentes pontos,
+    de nosso programa.
+
+    Em javaScript, denominam-se funções globais as que NÃO estão associadas a um objeto particular
+    da linguagem. As funções globais são.
 */
+
+encodeURI(uri);
+decodeURI(uri);
+encodeURIComponent(uri);
+decodeURIComponent(uri);
+escape(string);// removida da ECMAScript 3
+eval(codigo);//Recebe como argumento uma string. Evitar seu uso.
+isFinite(valor);
+isNaN(valor);
+Number(valor); // converte para numero alguma string com com um numero dentro.
+parseInt(2.5);// Pode receber uma strong também com um numero dentro.
+parseFloat();// Pode receber uma string também com um numero dentro.
+String();// Converte para string qualquer valor passado.
