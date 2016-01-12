a74040133681e322f3c92a42fae93994d31fd10d
Jairo Rudas <jairorudasp@gmail.com>
2016-01-12 00:20:11 -0200

    Explicação do objeto arguments em desenvolvimento.

---
+++ portugues/5-funções/argument.js
@@ -0,0 +1,24 @@
+/*
+    Variavel argument
+
+    A variavel arguments que possuim as funcões são semelhante a um array, contendo os
+    argumentos passados para a função. Não deve ser tratada como um array puro, pois não
+    soporta os metodos dos arrays, execeto .length. Contudo veremos que podemos transformar
+    este objeto em um objeto array puro.
+
+    Este objeto indexa argumentos a partir do zero, Assim podemos usar a sintaxe dos array
+    para referenciar elementos exemplo: array[0]. Argumentos só existe enquanto a funão esta sendo
+    executada e esta acessivel so no corpo da função. tentar chamar essa variavel fora da função
+    gera um erro.
+*/
+
+function calcular(b, g, h) {
+  console.log(arguments.length);//retorna a quantidade de argumentos que estão sendo passadas pra função
+  console.log(arguments[0]); // Retona 15
+  console.log(arguments[1]); // Retona 3
+  console.log(arguments[2]); // Retona 8
+  return (b * h)/g
+}
+
+var resultado = calcular(15, 3, 8);
+console.log(resultado);
