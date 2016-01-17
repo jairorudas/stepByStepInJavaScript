abac1f6e03b672281cbfef758f64b10f86942ed6
Jairo Rudas <jairorudasp@gmail.com>
2016-01-16 22:13:46 -0200

    Meted toString das funções

---
+++ portugues/5-funções/metodo_toString.js
@@ -0,0 +1,15 @@
+/*
+    Metodo toString().
+
+    Basicamente o que ele faz na funções é retornar ela como uma String.
+    vemos . . .
+
+*/
+
+function Cafe ( aroma, acidez, estado ) {
+  this.aroma  = aroma,
+  this.acidez = acidez,
+  this.estado = estado
+}
+
+Cafe.toString(); //Copia este codigo no console e você verá a string.
