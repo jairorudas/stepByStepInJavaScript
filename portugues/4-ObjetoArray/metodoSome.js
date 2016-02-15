89d45cfc8bf6df86552bb1d612463c0eac388de3
Jairo Rudas <jairorudas@Mac-mini-de-Jairo.local>
2016-02-15 15:15:35 -0200

    Mais um exemplo do método Some.

--- portugues/4-ObjetoArray/metodoSome.js
+++ portugues/4-ObjetoArray/metodoSome.js
@@ -1,4 +1,4 @@
-/*         Metodo some().
+/*      Metodo some().
 
         A função requer três argumentos que representam respectivamente o valor de cada elemento do array,
         o indice de cada elemento do array e o objeto array sendo percorrido.
@@ -7,20 +7,37 @@
         Para usar este metodo precisamos de uma função callback ou seja, ela é chamada e o trabalho dela é
          procesar nosso  Array com as caracteristica que queremos que sejam evaluadas.
 
-        A função some() testa cada um dos elementos do array e se AO MENOS UMELEMENTO satifaz a nossa função callback
+        A função some() testa cada um dos elementos do array e se AO MENOS UM ELEMENTO satifaz a nossa função callback
         retorna TRUE.Se nehum dos elementos de nosso array não satisfaz a função callback retorna FALSE.
 
-        Ele não altera a função array original.
+        Ele não altera a o array original.
 
 */
 
+// Exemplo 1
  var numeros = [25,85,"78"];
 
  function testNumeros(elem, ind, obj){
      return (typeof elem == "string");
-     /*rotorna TRUE
-     pois ao menos um deles satisfaz a condição.*/
+     /*
+        rotorna TRUE
+        pois ao menos um deles satisfaz a condição.
+    */
  }
+
  // Vamos a usar metodo some()
  var resultado = numeros.some(testNumeros);
  console.log(resultado); //Retorna TRUE
+
+// Exemplo 2
+var nomes = ["jairo", "patricia", "marcos", "carla", 7];
+
+function testeNome(elem, ind, obj){
+  if (nomes !== undefined) {
+    return typeof elem === "number";
+  }
+}
+
+var resposta = nomes.some(testeNome);
+console.log(resposta);
+// Seu retorno é TRUE, pois um elemento satisfaz a condição.
