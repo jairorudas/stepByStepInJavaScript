29adb659d563c6eb5f6d93e3d90faab76d255211
Jairo Rudas <jairorudas@Mac-mini-de-Jairo.local>
2016-02-17 11:11:38 -0200

    Método Melhorado na sua explicação.

--- portugues/4-ObjetoArray/metodoForEach.js
+++ portugues/4-ObjetoArray/metodoForEach.js
@@ -22,9 +22,12 @@
       Veremos exemplo a seguir
 */
 
+//---------------------------------------------------------------------------------------
+
 //Exemplo 1
 var edad = [21,3,18,"a", 290];
 var mensage;
+
 //criamos nossa função callback
 function andando (elem, ind, obj) {
   mensage += "\n" + "arr[" + ind + "] = " + elem + "\n";
@@ -33,7 +36,9 @@ function andando (elem, ind, obj) {
 
 //vamos colocar para funcionar
 edad.forEach(andando);
-console.log(mensage);
+console.log(typeof mensage);
+
+//---------------------------------------------------------------------------------------
 
 //Exemplo 2
 var saida,
@@ -48,13 +53,15 @@ function recorrendo (elem,ind,obj) {
   for (var k = 0; k <= oficina.length; k += 1) {
     if(typeof oficina[k] == "object"){
       for (var i in oficina[k]) {
-        objeto += i + " : " + oficina[k][i] + " \n"
+        objeto += i + " : " + oficina[k][i] + " \n";
       }
     }
   }
  }
 oficina.forEach(recorrendo);
-console.log(objeto);
+console.log( typeof objeto);
+
+//---------------------------------------------------------------------------------------
 
 //Exemplo 3
 var frutas = ["pepino", "pera", "ponche", "mamão", 22],
@@ -68,19 +75,23 @@ var frutas = ["pepino", "pera", "ponche", "mamão", 22],
       alert(elem + " Não é uma fruta, é um  " + naosei + " \n" + " ");
     }
 
-    /* NAO FAZER O SEGUIENTE DENTRO DE UM forEach(), SÓ SE VC SABE O QUE ESTÁ FAZENDO
-    Pois estará percorrendo o array de novo.
-      for (var i = 0; i == morango.length; i++) {
-        if (typeof morango[i] ==  "string"){
-          saida += morango[i] + " \n";
-          continue;
-        }else {
-          console.log("Não é uma string " + morango[i] + " \n");
+    /*
+        NAO FAZER O SEGUIENTE DENTRO DE UM forEach(), SÓ SE VC SABE O QUE ESTÁ FAZENDO
+        Pois estará percorrendo o array de novo.
+        for (var i = 0; i == morango.length; i++) {
+          if (typeof morango[i] ==  "string"){
+            saida += morango[i] + " \n";
+            continue;
+          }else {
+            console.log("Não é uma string " + morango[i] + " \n");
+          }
         }
-      }*/
+    */
   }
 frutas.forEach(callback);
 console.log(saida);
+
+//---------------------------------------------------------------------------------------
 
 //Exemplo 4
 /*
@@ -91,7 +102,8 @@ var frutas = ["pepino", "pera", "ponche", "mamão", 22],
     saida   = " ";
 
   function callback(elem, ind, obj){
-    if(typeof elem === "string"){ //esta linha é diferente ao exercicio anterior
+    if(typeof elem === "string"){
+      //esta linha é diferente ao exercicio anterior
       saida += elem + " \n";
     }else {
       var naoseioquee = typeof ind;
@@ -101,17 +113,21 @@ var frutas = ["pepino", "pera", "ponche", "mamão", 22],
 frutas.forEach(callback);
 console.log(saida);
 
-/*Exemplo 5
-  Este exemplo em especial, faz a mesma coisa que o exemplo 1 do metodoMap(),
-  mas o ideal seria usar o metodo map() para modificar os elementos do array.
-  A grande diferença etsa em que o metodo map devolve um novo array com as mudanças.
- já forEach() não. */
+//---------------------------------------------------------------------------------------
+
+/*  Exemplo 5
+    Este exemplo em especial, faz a mesma coisa que o exemplo 1 do metodoMap(),
+    mas o ideal seria usar o metodo map() para modificar os elementos do array.
+    A grande diferença etsa em que o metodo map devolve um novo array com as mudanças.
+   já forEach() não.
+*/
 
 var lego = [12, 23,45, 30,22];
 var fet = new Array;
 function tetri(elem, ind, obj){
   fet.push(elem = elem + 1);
   console.log(fet);
-  return fet
+  return fet;
 }
+
 lego.forEach(tetri);
