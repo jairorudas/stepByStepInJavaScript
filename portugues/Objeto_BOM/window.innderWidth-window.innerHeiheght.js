d787dd8ea426c72fa9d79d310ec753eb899d5ca3
Jairo Rudas <jairorudasp@gmail.com>
2016-04-15 23:02:02 -0300

    propiedades innerWidth e innerHeight explicadas

---
+++ portugues/Objeto_BOM/window.innderWidth-window.innerHeiheght.js
@@ -0,0 +1,20 @@
+/*
+    Objeto window.InnerWidth e window.innerHeight
+
+    Estás propiedade definem respectivamente a largura e a altura da àrea de conteudo
+    da janela do navegador. Não consideram as barras de Ferramentas, de Status, de Rolagem
+    e Menus.
+
+    Essas propiedades são suportadas epenas pelo nevagadore Firefox. Para
+    Internet Explorer a sintaxe é document.body.clientWidth e
+    document.body.clientHeight.
+
+*/
+
+//==============================================================================
+
+//Exercicio 1
+
+console.log(window.innerWidth + ' ' + window.innerHeight);
+// Ele retornará o o tamanho do window disponivel para usar no browser
+// Se você perguntar assim screen.width retornará em pixels o tamanho da tela do usuario.
