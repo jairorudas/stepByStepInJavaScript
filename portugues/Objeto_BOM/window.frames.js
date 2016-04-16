f1cf83333344ee61b7ba61820a01d6334b4b9893
Jairo Rudas <jairorudasp@gmail.com>
2016-04-15 22:37:40 -0300

    Objeto windows explicado e propiedade frames esplcada

---
+++ portugues/Objeto_BOM/window.frames.js
@@ -0,0 +1,22 @@
+/*
+    Objeto window.frames
+
+    Este metodo retorna a quantidade de frames e iframes contidas em um documento
+
+    vamos ver um exemplo:
+*/
+
+//==============================================================================
+
+//Exemplo 1
+/*
+  * Rodar na consola do browser o codigo a seguir e dectar quantos frames ou iframes o site
+    possui
+*/
+
+self.frames.lenght;
+window.frames.lenght;
+this.frames.lenght;
+frames.lenght;
+
+//Todoas linhas anteriores fazem o mesmo trabalho.
